8ef896782e3bc14702e1e7be38c3d1766c5f61d1
Wanderson Luiz <wwluiz17@gmail.com>
2025-08-10 11:35:51 +0000

    CURSOADS

--- CURSO ADS/supertrunfo.c
+++ CURSO ADS/supertrunfo.c
@@ -1,40 +1,87 @@
 #include <stdio.h>
 
-int main (){
+int main(){
 
-char estado[2];
-char carta[2];
-char codigo[2];
-char cidade[2];
-int populacao[2];
-float area;
-float pib[2];
-int pontosturisticos;
+    // CRIANDO CIDADE 01
 
-printf("Qual é a sua carta: \n");
-scanf("%s" , &carta);
+    char estado1[3], codigocarta1[4], nomecidade1[50]; // char do modo string - %c ou %s 
+    int populacao1, pontosturisticos1; // variaveis inteiras - %d
+    float area1, pib1; // flutuante - %f
 
-printf("Qual é o seu estado: \n");
-scanf("%s" , &estado);
+    // Cadastrando Cartas Cidade 01
 
-printf("Qual é o seu código: \n");
-scanf("%s" , &codigo);
+    printf("Código da Carta: ");
+    scanf("%s", codigocarta1);  
 
-printf("Qual e o nome da cidade: \n");
-scanf("%c" , &cidade);
+    printf("Estado: ");
+    scanf("%s", estado1);    
 
-printf("Número da População: \n");
-scanf("%d" , &populacao);
+    printf("Nome da Cidade: ");
+    scanf("%s" , nomecidade1);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao1);
+
+    printf("Area: ");
+    scanf("%f", area1);
+
+    printf("PIB: ");
+    scanf("%f", pib1);
+
+    printf("Pontos Turisticos: ");
+    scanf("%d", pontosturisticos1);
+
+    // EXIBIÇÃO DAS CARTAS 01
+
+    
+    printf("Código da Carta: %s\n", codigocarta1);
+    printf("Estado: %s\n", estado1);
+    printf("Nome da Cidade: %s\n", nomecidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Pontos Turisticos: %d\n", pontosturisticos1);
 
 
 
+ // CRIANDO CIDADE 02
 
+    char estado2[3], codigocarta2[4], nomecidade2[50]; // char do modo string - %c ou %s 
+    int populacao2, pontosturisticos2; // variaveis inteiras - %d
+    float area2, pib2; // flutuante - %f
 
+ // Cadastrando Cartas Cidade 02
 
+    printf("Código da Carta: ");
+    scanf("%s", codigocarta2);  
 
+    printf("Estado: ");
+    scanf("%s", estado2);    
 
+    printf("Nome da Cidade: ");
+    scanf("%s" , nomecidade2);
 
+    printf("Populacao: ");
+    scanf("%d", populacao2);
 
-return 0;
+    printf("Area: ");
+    scanf("%f", area2);
 
+    printf("PIB: ");
+    scanf("%f", pib2);
+
+    printf("Pontos Turisticos: ");
+    scanf("%d", pontosturisticos2);
+
+// EXIBIÇÃO DAS CARTAS 02
+
+    printf("Código da Carta: %s\n", codigocarta2);
+    printf("Estado: %s\n", estado2);
+    printf("Nome da Cidade: %s\n", nomecidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Pontos Turisticos: %d\n", pontosturisticos2);
+
+    return 0;
 }
